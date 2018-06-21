a0b6e63c8ed6b74bbf31b29522827cfca9308771
AndresED <andres30xed@gmail.com>
2018-06-20 22:40:13 -0500

    Conexion al socket

--- server/index.js
+++ server/index.js
@@ -3,9 +3,21 @@
  var server= require('http').Server(app);
  var io = require('socket.io')(server);
  var port=6677;
+
+app.use(express.static('client'));
+
+
  app.get('/',function(req,res){
  	res.status(200).send('Hola mundo desde una ruta');
  });
+
+
+io.on('connection',function(socket){
+	console.log("El nodo con IP: "+socket.handshake.address+" se ha conectado");
+});
+
+
+
  server.listen(port,function(){
  	console.log("Servidor esta  funcionando en  http://localhost:"+port);
  });
